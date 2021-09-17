f21f3a6ceb650dee93594b6512cc0cce17644c6a
fuatguzel <fuatguzel754@gmail.com>
2021-09-17 20:55:10 +0300

    Add ToDo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
-import Counter from './components/Counter'
+//import Counter from './components/Counter'
+import ToDo from './components/ToDo'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter /> */}
+      <ToDo />
     </div>
   );
 }
---
+++ src/components/ToDo/index.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+
+
+const defaultItems = [
+  {
+    name: "Item 1"
+  },
+  {
+    name: "Item 2"
+  },
+  {
+    name: "Item 3"
+  }
+]
+
+function ToDo() {
+  const [text, setText] = useState("")
+  const [items, setItems] = useState(defaultItems)
+
+  const addItem = () => {
+    setItems((prev) => [...prev, {name : text}])
+    setText("")
+  }
+
+  return (
+    <div>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addItem}>Add</button>
+      <br/><br/>
+      {
+        items.map((item, key) => (
+          <div key={key}>{item.name}</div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default ToDo
